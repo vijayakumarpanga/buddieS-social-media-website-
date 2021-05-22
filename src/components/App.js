62c86d69c2ae24aba4bb15665cad1f0bf53578d7
VIJAYAKUMAR <pangavijaykumar9966@gmail.com>
2021-05-22 16:17:36 +0530

    Add Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Post from './Post'
 import {connect} from 'react-redux'
 import '../css/post.css'
 import {fetchPosts} from '../actions/posts'
+import Navbar from './Navbar';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts())
@@ -12,9 +14,14 @@ class App extends Component {
   render() {
     console.log("inside App",this.props.posts)
     return (
+      <Router>
       <div>
-        <Post ></Post>
+        {/* <Navbar></Navbar>
+        <Post ></Post> */}
+        <Route path="/" component={Navbar}></Route>
+        <Route path="/" component={Post}/>
       </div>
+      </Router>
     );
   }
 }
